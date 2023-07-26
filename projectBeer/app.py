04924ba5078bb282ebed261e5c52b044d19f1844
Husted42 <alx2608@gmail.com>
2023-07-26 08:38:51 +0200

    add donut

--- projectBeer/app.py
+++ projectBeer/app.py
@@ -71,6 +71,34 @@ def avgByBrew():
     plt.xticks(rotation = 45)
     plt.savefig('static/plots/avgByBrew.png')
 
+'''Saves a plot in static/plots/'''
+def donoutChart():
+    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+    data = createDataframe()
+    data = data['country'].value_counts().reset_index()
+    data = data.sort_values(by=['count'], ascending=False)
+    data.loc[data['count'] < 3, 'country'] = "other"
+    data = data.groupby(['country'])['count'].sum().reset_index()
+    labels = list(data.pop('country'))
+    count = list(data.pop('count'))
+    wedges, texts = ax.pie(count, wedgeprops=dict(width=0.5), startangle=-40)
+
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+        bbox=bbox_props, zorder=0, va="center")
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = f"angle,angleA=0,angleB={ang}"
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(labels[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+            horizontalalignment=horizontalalignment, **kw)
+
+    ax.set_title("Distribution of beer count by country", y=1.05)
+    plt.savefig('static/plots/donoutChart.png')
 
 ##### ----- background scheduler ----- #####
 # Create a background scheduler, that runs every hour: 
@@ -136,6 +164,7 @@ if __name__ == "__main__":
     #Function to run on startup
     avgByBrew()
     avgByCoun()
+    donoutChart()
     app.run(debug=True)
 
 ############### ---------- Section ---------- ###############
