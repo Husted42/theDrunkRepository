6f207c82c8135199d52168f6141145154e9dccb4
Husted42 <alx2608@gmail.com>
2023-07-27 14:51:36 +0200

    add logos

--- projectBeer/sqlQueries/selection.sql
+++ projectBeer/sqlQueries/selection.sql
@@ -6,10 +6,15 @@ WHERE LOWER(brewer)
 LIKE LOWER('%han%');
 
 SELECT * FROM Beer WHERE country = 'denmark'
-AND 8 <= alc AND alc <= 15;
+AND 0 <= alc AND alc <= 8;
 
 SELECT * FROM account;
 
-SELECT brewer, name, alc, CONCAT(UPPER(LEFT(country,1)),LOWER(RIGHT(country,LENGTH(country)-1))), rating FROM Beer;
+SELECT brewer, name, alc, 
+CONCAT(UPPER(LEFT(country,1)),LOWER(RIGHT(country,LENGTH(country)-1))), rating FROM Beer;
 
-SELECT brewer, name, alc, CONCAT(UPPER(LEFT(country,1)),LOWER(RIGHT(country,LENGTH(country)-1))), rating FROM Beer WHERE LOWER(name) LIKE LOWER('%guld%') ORDER BY rating DESC;
+SELECT brewer, name, alc, 
+CONCAT(UPPER(LEFT(country,1)),LOWER(RIGHT(country,LENGTH(country)-1))), rating FROM Beer 
+WHERE LOWER(name) LIKE LOWER('%guld%') ORDER BY rating DESC;
+
+SELECT * FROM Beer WHERE brewer = 'Biermischgetränk';
